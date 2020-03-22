b70e5c7b9bb43ec4d8fd1b39987f46a99782ba50
Livia Boeriu <boeriulivia@gmail.com>
2020-03-22 14:01:01 +0100

    things kind of work

--- functions/index.js
+++ functions/index.js
@@ -14,23 +14,50 @@ const app = dialogflow({
   });
 
 app.intent('Game', (conv) => {
-  conv.ask("Ok, lets play two truths one lie game. Haha, kidding, you are going to do most of the work. Each of you think of of three statements out of which one is wrong. Then say correct or incorrect when the other person tries to guess.");
+  conv.ask("Ok, lets play two truths one lie. Haha, kidding! You are going to do most of the work. Each of you should think of three statements out of which one is false.");
+  conv.ask("After the other person tries to guess, just say correct after they got the right answer. Are you ready to begin?");
 });
 
-app.intent('FirstStatement', (conv) => {
+app.intent('GetStatements', (conv, params) => {
+  conv.user.storage.firstStatement = conv.parameters.first;
+  var firstStatement = conv.user.storage.firstStatement;
 
+  conv.user.storage.secondStatement = conv.parameters.second;
+  var secondStatement = conv.user.storage.secondStatement;
+
+  conv.user.storage.thirdStatement = conv.parameters.third;
+  var thirdStatement = conv.user.storage.thirdStatement;
+
+  conv.ask(`Super, now you have to guess which is the false statement!`);
+});
+
+
+app.intent('The Answer Is', (conv) => {
+  conv.followup(`what-do-you-think`);
+});
+
+app.intent('Which is correct', (conv) => {
+  conv.ask('Is that the correct one?');
 });
 
 app.intent('Correct', (conv) => {
-  conv.ask('Yey, that was a good one! Would you like to play another round?');
+  conv.ask('Do you want to try again?');
 });
 
 app.intent('Incorrect', (conv) => {
   conv.ask('Awww, not this time around. Would you like to try again?');
 });
 
-app.intent('Correct try again', (conv) => {
-  conv.followup('tryagain');
+app.intent('Try again', (conv) => {
+  conv.followup(`get-statements`);
+});
+
+app.intent('End Game', (conv) => {
+  conv.ask('Ok, no problem! Let me know if you want to try the conversation mode by sayind: conversation, or if you want to quit.')
+});
+
+app.intent('Test try again', (conv) => {
+  conv.followup(`tryagain`);
 });
   
 // Set the DialogflowApp object to handle the HTTPS POST request.
